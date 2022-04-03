4e327dbfd8d33af7c0dc0c610563453f75b762ea
bryceworth <bryceaworth@gmail.com>
2022-04-03 16:23:51 -0700

    commit tree 29dbb9a90ffe60d302d1d8f75562879a72364e7d

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solutions in this file
+//for (let age = 30; age < 40; age ++) {
+ //   console.log(`I'm ${age} years old. Happy birthday to me!`);
+//}
+  
+
+/* const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Thank you ${gifts[i]} for the wonderful surprise gift!`);
+
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts); */
+
+
+//const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, word) {
+    const thankyouCards = []
+      for (let i = 0; i < names.length; i++) {
+   thankyouCards.push(`Thank you, ${names[i]}, for the wonderful ${word} gift!`)
+      }
+      return thankyouCards
+    }
+  
+    function countDown(number) {
+  
+        while (number >= 0){
+     console.log (number)
+    number--
+      }}
